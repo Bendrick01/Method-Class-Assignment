7d407064e0e6a2a5c54e0689f82c5d23d233353b
Bendrick01 <131765016+Bendrick01@users.noreply.github.com>
2024-03-03 09:48:36 +0000

    Add project files.

---
+++ Method Class Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Creating the class Maths
+public class Maths
+{
+    // Void method taking two integers as parameters
+    public void PerformMathOperation(int firstNumber, int secondNumber)
+    {
+        // Maths operation on the first integer
+        int result = firstNumber + 1;
+
+        // Display the second integer
+        Console.WriteLine($"Result: {result}, Second Number: {secondNumber}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate Maths class
+        Maths mathsObject = new Maths();
+
+        // Calling the method and passing numbers
+        mathsObject.PerformMathOperation(50, 100);
+
+        // Calling the method and parameters
+        mathsObject.PerformMathOperation(firstNumber: 20, secondNumber: 75);
+    }
+}
